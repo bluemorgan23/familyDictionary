de0e3f565740e88bff7a7d44440ae016d7829625
Chris Morgan <bluemorgan2323@gmail.com>
2019-05-28 18:51:35 -0500

    First commit, done exercise

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace family_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>(){
+                {"name", "Krista"},
+                {"age", "42"}
+            });
+
+            myFamily.Add("brother", new Dictionary<string, string>(){
+                {"name", "Cannon"},
+                {"age", "7"}
+            });
+
+            myFamily.Add("mom", new Dictionary<string, string>(){
+                {"name", "Jen"},
+                {"age", "43"}
+            });
+
+            foreach(KeyValuePair<string, Dictionary<string, string>> familyMember in myFamily)
+            {
+                Console.WriteLine($"{familyMember.Value["name"]} is my {familyMember.Key} and is {familyMember.Value["age"]} years old.");
+            }
+        }
+    }
+}
